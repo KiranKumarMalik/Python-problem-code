5388766601214d9f1d25088c2df699b1fe388e31
KiranKumarMalik <malikkiran413@gmail.com>
2025-01-13 22:03:39 +0530

    new code

--- Multithreading/example.py
+++ Multithreading/example.py
@@ -7,7 +7,7 @@ def boys():
 def girls():
     for num in range(11,16):
         print(num)
-        time.sleep(1)
+        time.sleep(1) 
 T1 = threading.Thread(target=boys)
 T2 = threading.Thread(target=girls)
 T1.start()
---
+++ test2.py
@@ -0,0 +1,13 @@
+var1="Kiran"
+var2=56
+print(var1+str(var2))
+
+S=["Kiran", "Kumar", "Ravi", "Rajesh"]
+print("".join(S))
+print(S.pop(3))
+print(S.extend(["Rajesh", "Ravi"]))
+print(S.insert(2, "Sagar"))
+print(S)
+# JSON: it is used to convert the python language to common language (JSON laguage ) anfd json to python langiuage
+# JSON: JavaScript Object Notation (JSON) is a standard text-based format for representing
+# structured data based on JavaScript object syntax. It is commonly used for transmitting data in web applications
